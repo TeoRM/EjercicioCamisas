e712ad7324ae2921042c4c86e0b65531431f33c4
TeoRM <107431128+TeoRM@users.noreply.github.com>
2023-06-27 17:15:27 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const number = parseFloat(document.getElementById("catidadCompra").value)
+
+function compraCamisas() {
+    let number = parseFloat(document.getElementById("catidadCompra").value)
+    let resultElement = document.getElementById("mensaje")
+
+    if (number > 5) {
+        resultElement.textContent = "La compra se debe pagarse con tarjeta de credito porfavor."
+
+    } else if (number <= 5 && number > 0) {
+        resultElement.textContent = "La compra se debe pagarse en efectivo porfavor."
+    } else {
+        resultElement.textContent = "Si deseas realizar alguna compra debe ser minima de 1 unidad."
+    }
+
+}
+
+
+
+
+
+
