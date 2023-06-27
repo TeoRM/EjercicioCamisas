7894685a490f939c798887253e50447eb839b188
TeoRM <107431128+TeoRM@users.noreply.github.com>
2023-06-27 17:24:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function compraCamisas() {
     let resultElement = document.getElementById("mensaje")
 
     if (number > 5) {
-        resultElement.textContent = "La compra se debe pagarse con tarjeta de credito porfavor."
+        resultElement.textContent = "La compra se debe pagar con tarjeta de credito porfavor."
 
     } else if (number <= 5 && number > 0) {
-        resultElement.textContent = "La compra se debe pagarse en efectivo porfavor."
+        resultElement.textContent = "La compra debe pagarse en efectivo porfavor."
     } else {
         resultElement.textContent = "Si deseas realizar alguna compra debe ser minima de 1 unidad."
     }
